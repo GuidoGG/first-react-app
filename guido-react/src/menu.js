272f642f68425728c9ed9a1d2e63ec72884942b0
GuidoGG <guidogottenbos@gmail.com>
2021-03-16 16:16:15 +0100

    init

---
+++ guido-react/src/menu.js
@@ -0,0 +1,9 @@
+export default function Example() {
+    return (
+        <Menu menuButton={<MenuButton>Open menu</MenuButton>}>
+            <MenuItem>New File</MenuItem>
+            <MenuItem>Save</MenuItem>
+            <MenuItem>Close Window</MenuItem>
+        </Menu>
+    );
+}
