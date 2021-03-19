8307fb2f29699cd54e21c89d5ac443284f452347
GuidoGG <guidogottenbos@gmail.com>
2021-03-19 12:56:00 +0100

    init

---
+++ guido-react/src/components/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+    render () {
+        return (
+        <>
+            <h2>Pagina 2</h2>
+        <p> Dit is een andere pagina</p>
+        </>
+    );
+    }
+}
+
+export default AboutPage;
---
+++ guido-react/src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+    <div className ="jumbotron">
+        <h1>Guido's mooie website</h1>
+    <p> Hier laat ik zien wat ik allemaal kan met React</p>
+  <a href= "/about">About</a>
+  
+    </div>
+)
+    };
+
+export default HomePage;
