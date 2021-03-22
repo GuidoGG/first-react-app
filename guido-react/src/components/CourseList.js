02f1d6bcfb4afcd3f972af7264acc9e7c238698f
GuidoGG <guidogottenbos@gmail.com>
2021-03-22 16:40:09 +0100

    init

--- guido-react/src/components/CourseList.js
+++ guido-react/src/components/CourseList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import {Link} from "react-router-dom";
 
 function CourseList(props){
     return (
@@ -13,14 +13,24 @@ function CourseList(props){
             </tr>
         </thead>
         <tbody>
-            
+            {props.courses.map(course => {
+                return (
+                    <tr key={course.id}>
+                        <td><Link to={"/course/" + course.slug}> {course.title}</Link></td>
+                        <td>{course.authorId}</td>
+                        <td>{course.category}</td>
+                    </tr>
+                );
+            })}
         </tbody>
     </table>
     )
 }
 
 CourseList.propTypes = {
-
-}
+    courses: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired
+    })).isRequired
+};
 
 export default CourseList;
