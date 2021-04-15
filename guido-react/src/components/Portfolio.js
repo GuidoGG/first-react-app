0a1765c91eef0e27043699ad9a98b2b97d48c808
GuidoGG <guidogottenbos@gmail.com>
2021-04-15 12:49:19 +0200

    init

--- guido-react/src/components/Portfolio.js
+++ guido-react/src/components/Portfolio.js
@@ -2,9 +2,24 @@ import React from "react"
 
 function Portfolio(){
     return (
+        <div>
+        <div>
         <h1>
-            Dit is mijn portfolio
+            Dit is mijn portfolio.
         </h1>
+        <p>
+            Hier komen voorbeelden van mijn React kunsten te staan.
+        </p>
+        <br>
+        </br>
+        <p>
+            Hoe ziet dit eruit?
+        </p>
+        </div>
+        <div style={{color:"red"}}>
+            Een rode div in een div
+        </div>
+        </div>
     )
 }
 
